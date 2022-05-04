34f99d753dda8e4d237a75824f558af136578a53
jerry <andreiturcu2005.24@gmail.com>
2022-05-04 11:04:52 -0700

    meniu no art

---
+++ powercore/ScenesNScripts/Menu/Scripts/MainMenu.gd
@@ -0,0 +1,53 @@
+extends MarginContainer
+
+# 1-start
+# 2-how to
+# 3-exit
+
+const first_scene = preload("res://ScenesNScripts/CharacterTest/Character.tscn")
+const second_scene = preload("res://ScenesNScripts/CharacterTest/Character.tscn")
+
+onready var selector_one = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Selector
+onready var selector_two = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/Selector
+onready var selector_three = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Selector
+
+var current_selection =-1
+
+func _ready():
+	set_current_selection(-1)
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_down") and current_selection<2:
+#		print("dadadadad")
+		current_selection += 1
+		set_current_selection(current_selection)
+	elif Input.is_action_just_pressed("ui_up") and current_selection>0:
+		current_selection -= 1
+		set_current_selection(current_selection)
+	elif Input.is_action_just_pressed("ui_accept"):
+		handle_selection(current_selection)
+	
+	
+func handle_selection(current_selection):
+	if(current_selection==0):
+		get_parent().add_child(first_scene.instance())
+		queue_free()
+	elif(current_selection==1):
+		get_parent().add_child(second_scene.instance())
+		queue_free()
+	elif(current_selection==2):
+		get_tree().quit()
+			
+			
+	
+	
+func set_current_selection(current_selection):
+	selector_one.text = ""
+	selector_two.text = ""
+	selector_three.text = ""
+	if( current_selection == 0 ):
+		selector_one.text ='->'
+	elif( current_selection == 1 ):
+		selector_two.text ='->'
+	elif( current_selection == 2 ):
+		selector_three.text ='->'
